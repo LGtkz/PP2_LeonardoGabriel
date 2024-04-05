8c53b7718fc02c6f275d308ec9c4000f3f9c6d1a
LGtkz <leozingabr@gmail.com>
2024-04-04 22:29:40 -0300

    fdmnsofds

--- js/form.js
+++ js/form.js
@@ -1,6 +1,12 @@
 const btn = document.getElementById('btn');
 const modal = new bootstrap.Modal(document.querySelector('.modal'));
-var valido = false
+var nomevalido = false;
+var emailvalido = false;
+var sexovalido = false;
+var idadevalido = false;
+var txtvalido = false;
+
+var sexo;
 
 const nome = document.getElementById('nome');
 const spanNome = document.getElementById('spanNome');
@@ -20,60 +26,101 @@ const reclamacao = document.getElementById("reclamacao");
 const criticaConstrutiva = document.getElementById("critica-construtiva");
 const encontreiBug = document.getElementById("encontrei-bug");
 const alerta = document.getElementById("alerta");
+
 alerta.style.display = "none";
 
+const nomeModal = document.getElementById("nomeModal");
+const idadeModal = document.getElementById("idadeModal");
+const emailModal = document.getElementById("emailModal");
+const sexoModal = document.getElementById("sexoModal");
+const contatoModal = document.getElementById("contatoModal");
+const textModal = document.getElementById("textModal");
+
+const textArea = document.getElementById("txA");
+const txSpan = document.getElementById("txSpan");
+
+const limpa = document.getElementById("limpa");
 btn.onclick = (e) => {
+     let valido = false;
     const sobrenome = nome.value.split(" ")[1];
-    if(nome.value.trim() == "" || sobrenome === undefined || nome.value.length < 4){
+    if (nome.value.trim() == "" || sobrenome === undefined || nome.value.length < 4) {
         spanNome.innerHTML = "Insira o nome completo";
         spanNome.style.display = "block";
         spanNome.style.color = "red";
         nome.style.border = "red solid 1px";
-        valido = false;
+        nomevalido = false;
     }
-    else{
+    else {
         spanNome.style.display = "none";
         nome.style.border = "solid 1px green";
-        valido = true;
+        nomevalido = true;
     }
-    if(!validateEmail(email.value)){
+    if (!validateEmail(email.value)) {
         spanEmail.innerHTML = "Insira o email correto";
         spanEmail.style.display = "block";
         spanEmail.style.color = "red";
         email.style.border = "red solid 1px";
-        valido = false;
+        emailvalido = false;
     }
-    else{
+    else {
         spanEmail.style.display = "none";
         email.style.border = "solid 1px green";
-        valido = true;
+        emailvalido = true;
     }
-    if(idade.value <= 0 || idade.value > 99 || idade.value == ""){
+    if (!masc.checked && !fem.checked) {
+        sexoSpan.innerHTML = "Selecione uma das opções"
+        sexoSpan.style.display = "block"
+        sexoSpan.style.color = "red"
+        sexovalido = false;
+    }
+    else {
+        sexoSpan.style.display = "none"
+        if(masc.checked){
+            sexo = "Masculino"
+        }
+        else{
+            sexo = "Feminino"
+        }
+        sexovalido = true;
+    }
+    if (idade.value <= 0 || idade.value > 99 || idade.value == "") {
         spanIdade.innerHTML = "Insira uma idade correta";
         spanIdade.style.display = "block";
         spanIdade.style.color = "red";
         idade.style.border = "red solid 1px";
-        valido = false;
+        idadevalido = false;
     }
-    else{
+    else {
         spanIdade.style.display = "none";
         idade.style.border = "solid 1px green";
-        valido = true;
+        idadevalido = true;
     }
-    if(!masc.checked && !fem.checked){
-        sexoSpan.innerHTML = "Selecione uma das opções"
-        sexoSpan.style.display = "block"
-        sexoSpan.style.color = "red"
+    if (textArea.value == "") {
+        txSpan.innerHTML = "Preencha com texto"
+        txSpan.style.color = "red"
+        txSpan.style.display = "block"
+        textArea.style.border = "red solid 1px"
+        txtvalido = false;
     }
-    else{
-        sexoSpan.style.display = "none"
+    else {
+        txSpan.style.display = "none"
+        textArea.style.border = "green solid 1px"
+        txtvalido = true;
     }
-    if(!duvida.checked && !reclamacao.checked && !criticaConstrutiva.checked && !encontreiBug){
-        alerta.style.display = "block";
-    }
-    else{
-        alerta.style.display = "none"
-    }
+    if (nomevalido && emailvalido && idadevalido && txtvalido){
+            valido = true;
+        }
+        if (!valido) {
+        }
+        else {
+            modal.show()
+            nomeModal.innerHTML = "Nome: " + nome.value;
+            idadeModal.innerHTML = "Idade: " + idade.value;
+            emailModal.innerHTML = "E-mail: " + email.value;
+            sexoModal.innerHTML = "Sexo: " + sexo;
+            contatoModal.innerHTML = "Escolhas: " + verificaContato();
+            textModal.innerHTML = "Texto: " + textArea.value;
+        }
 
 }
 
@@ -81,4 +128,30 @@ function validateEmail(email) {
     const regex = /^((?!@a\.))([^\s@]+)@((?:[a-z0-9-]+\.)+[a-z]{2,6})$/;
     const match = regex.test(email);
     return match;
+}
+
+function verificaContato(){
+    let escolhas = " ";
+    if(duvida.checked){
+        escolhas = escolhas + "Dúvidas "
+    }
+    if(reclamacao.checked){
+        escolhas = escolhas + "Reclamações "
+    }
+    if(criticaConstrutiva.checked){
+        escolhas = escolhas + "Critica construtiva "
+    }
+    if(encontreiBug.checked){
+        escolhas = escolhas + "Encontrei Bug\n"
+    }
+    return escolhas;
+}
+
+limpa.onclick = () => {
+    nomeModal.innerHTML = "Nome: "
+    idadeModal.innerHTML = "Idade: "
+    emailModal.innerHTML = "E-mail: "
+    sexoModal.innerHTML = "Sexo: "
+    contatoModal.innerHTML = "Escolhas: "
+    textModal.innerHTML = "Texto: "
 }
